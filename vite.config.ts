d1d1abf38647c6005853238e1edaf9dca39b69c7
samsularifin05 <samsul026@gmail.com>
2025-04-22 09:38:33 +0700

    update latest package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig(({ command }) => {
   return {
-    plugins: [vue()],
-    resolve: {
-      alias: [
-        {
-          find: "@vue/runtime-core",
-          replacement: "@vue/runtime-core/dist/runtime-core.esm-bundler.js"
-        }
-      ]
-    }
+    plugins: [vue()]
   };
 });
